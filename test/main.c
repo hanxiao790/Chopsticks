f0b2dbdd13a331ed1de9e1667e6c5a0e07097c7c
Akira <hanxiao790@gmail.com>
2015-11-03 14:44:17 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by HanXiao on 11/3/15.
+//  Copyright © 2015 HanXiao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
